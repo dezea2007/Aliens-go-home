02b1faa8f14a53282e0af457ea1022ac5e797a3e
dezea2007 <48436409+dezea2007@users.noreply.github.com>
2022-02-13 19:00:49 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>We will create an awesome game with react, redux and SVG</h2>
+    </div>
+  );
+}
+
+export default App;
